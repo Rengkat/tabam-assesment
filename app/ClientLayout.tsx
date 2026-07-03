ebd651cc44a112bd5953200f281f97b6f43463a2
Rengkat <successinmotion11@gmail.com>
2026-07-03 23:30:48 +0100

    install epenecies and add toast

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,27 +1,13 @@
 import type { Metadata } from "next";
 import { Toaster } from "sonner";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 export const metadata: Metadata = {
   title: "Ecommerce Admin Dashboard",
   description: "Manage products, categories, and inventory",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
